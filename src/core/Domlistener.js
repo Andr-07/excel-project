a47470e877218f400759104180299380086224b1
Andrey <aa.lubochnikov@gmail.com>
2020-05-09 23:04:00 +0300

    feature(framework)-bases

--- src/core/Domlistener.js
+++ src/core/Domlistener.js
@@ -1,8 +1,33 @@
+import { capitalize } from "./utils"
+
 export class Domlistener {
-    constructor($root) {
+    constructor($root, listeners = []) {
         if (!$root) {
-            throw new Error ('No $root provided for DomListener')
+            console.error('No $root provided for DomListener')
         }
         this.$root = $root
+        this.listeners = listeners
     }
+
+    initDOMListeners(){  
+        this.listeners.forEach(listener => {
+            const method = getMethodName(listener)
+            if (!this[method]) {
+                console.error(`Method ${method} is not  implemented in ${this.name} Component`)
+            }
+            this[method] = this[method].bind(this)
+            this.$root.on(listener, this[method])
+        })
+    }
+
+    removeDOMListeners(){
+        this.listeners.forEach(listener => {
+            const method = getMethodName(listener)
+            this.$root.off(listener, this[method])
+        })
+    }
+}
+
+function getMethodName(eventName) {
+    return 'on' + capitalize(eventName)
 }
